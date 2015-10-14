3500b0cd6f9b19b06cf199b776691001edaf6ddb
Robert Brown <ammoknight@gmail.com>
2015-10-13 22:38:13 -0600

    Bump version number

--- mix.exs
+++ mix.exs
@@ -3,29 +3,17 @@ defmodule SymbolicExpression.Mixfile do
 
   def project do
     [app: :symbolic_expression,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
